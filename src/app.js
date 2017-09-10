983023a99ba92161aaee9b316e9af7064bf456c9
EienKotonaru <mogilnikoff2@gmail.com>
2017-09-10 18:54:08 +0300

    initial

--- src/app.js
+++ src/app.js
@@ -1,5 +1,51 @@
-import "./index.html";
-import "./style.scss";
-import "./images/logo.jpg";
+'use strict';
 
-console.log(1);
+const application = document.getElementById('application');
+
+const liveSectionsCollection = application.getElementsByTagName('section');
+const liveSectionsArray = Array.from(liveSectionsCollection);
+
+const signup = document.getElementsByClassName('signup')[0];
+const records = document.getElementsByClassName('records')[0];
+const about = document.getElementsByClassName('about')[0];
+const mainMenu = document.getElementsByClassName('main')[0];
+
+mainMenu.onclick = function () {
+    let curr = liveSectionsArray
+        .find(section => section.hidden === false)
+        .hidden = true;
+
+    let next = liveSectionsArray
+        .find(section => section.id === 'login')
+        .hidden = false;
+
+    mainMenu.hidden = true;
+};
+
+signup.onclick = function () {
+    event.target.parentElement.hidden = true;
+    const nextSection = liveSectionsArray
+        .find(section => section.id === 'signup');
+
+    nextSection.hidden = false;
+    mainMenu.hidden = false;
+};
+
+records.onclick = function () {
+    event.target.parentElement.hidden = true;
+    const nextSection = liveSectionsArray
+        .find(section => section.id === 'records');
+
+    nextSection.hidden = false;
+    mainMenu.hidden = false;
+};
+
+
+about.onclick = function () {
+    event.target.parentElement.hidden = true;
+    const nextSection = liveSectionsArray
+        .find(section => section.id === 'about')
+        .hidden = false;
+
+    mainMenu.hidden = false;
+};
