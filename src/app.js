896b0c51f8eb4584540cef555f8edcc91e4ccad1
EienKotonaru <mogilnikoff2@gmail.com>
2017-09-12 15:34:29 +0300

    merge layout

--- src/app.js
+++ src/app.js
@@ -13,42 +13,48 @@ const records = document.getElementsByClassName('records')[0];
 const about = document.getElementsByClassName('about')[0];
 const mainMenu = document.getElementsByClassName('main')[0];
 
+const setSectionHidden = (id, bool) => liveSectionsArray
+    .find(section => section.id === id)
+    .hidden = bool;
+
 mainMenu.onclick = function () {
     liveSectionsArray
         .find(section => section.hidden === false)
         .hidden = true;
-
-    liveSectionsArray
-        .find(section => section.id === 'login')
-        .hidden = false;
-
+    setSectionHidden('login', false);
     mainMenu.hidden = true;
 };
 
 signup.onclick = function () {
-    event.target.parentElement.hidden = true;
-    liveSectionsArray
-        .find(section => section.id === 'signup')
-        .hidden = false;
-
+    setSectionHidden('login', true);
+    setSectionHidden('signup', false);
     mainMenu.hidden = false;
 };
 
 records.onclick = function () {
-    event.target.parentElement.hidden = true;
-    const nextSection = liveSectionsArray
-        .find(section => section.id === 'records')
-        .hidden = false;
-
+    setSectionHidden('login', true);
+    setSectionHidden('records', false);
     mainMenu.hidden = false;
 };
 
 
 about.onclick = function () {
-    event.target.parentElement.hidden = true;
-    const nextSection = liveSectionsArray
-        .find(section => section.id === 'about')
-        .hidden = false;
-
+    setSectionHidden('login', true);
+    setSectionHidden('about', false);
     mainMenu.hidden = false;
+};
+
+const email = document.getElementsByName('email')[0];
+
+email.onchange = function () {
+    if(!/^\w+@[a-z]+\.[a-z]+$/g.test(email.value))
+        alert('Invalid email');
+};
+
+const passwordsCollection = document.getElementsByName('password');
+const passwordsArray = Array.from(passwordsCollection);
+
+passwordsArray[0].onchange = function () {
+    if (passwordsArray[0].value.length < 8)
+        alert('Password must be at least 8 characters long');
 };
