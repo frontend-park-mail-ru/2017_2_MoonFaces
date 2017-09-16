c7bd1b45e5f998bdc46c5e927c936a76c3a8f014
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-09-16 11:29:52 +0300

    base pages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import './index.html';
 import './style.scss';
 import './images/background.jpg'
 import './fonts/consola.ttf'
+'use strict';
+
 
 const app = (function () {
     const importedNavigation = require('./js/navigation');
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -7,7 +7,7 @@ const liveSectionsArray = Array.from(liveSectionsCollection);
 const signup = document.getElementsByClassName('signup')[0];
 const records = document.getElementsByClassName('records')[0];
 const about = document.getElementsByClassName('about')[0];
-const mainMenu = document.getElementsByClassName('main')[0];
+const mainMenuList = document.getElementsByClassName('main');
 
 const setSectionHidden = (id, display) => liveSectionsArray
     .find(section => section.id === id)
@@ -16,16 +16,16 @@ const setSectionHidden = (id, display) => liveSectionsArray
 const transitFromMainMenu = sectionName => {
     setSectionHidden('login', 'none');
     setSectionHidden(sectionName, 'block');
-    mainMenu.style.display = 'block';
 };
 
-mainMenu.onclick = () => {
-    liveSectionsArray
-        .find(section => section.style.display === 'block')
-        .style.display = 'none';
-    setSectionHidden('login', 'block');
-    mainMenu.style.display = 'none';
-};
+for(let mainMenu of mainMenuList){
+    mainMenu.onclick = () => {
+        liveSectionsArray
+            .find(section => section.style.display === 'block')
+            .style.display = 'none';
+        setSectionHidden('login', 'block');
+    };
+}
 
 signup.onclick = () => transitFromMainMenu('signup');
 records.onclick = () => transitFromMainMenu('records');
