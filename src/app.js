87cca908d2beb4111ccb10d36d0bf0bc9bd92997
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-09-18 11:50:37 +0300

    semicolon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import './index.html';
 import './style.scss';
-import './images/background.jpg'
-import './fonts/consola.ttf'
+import './images/background.jpg';
+import './fonts/consola.ttf';
 
 const app = (function () {
     const importedNavigation = require('./js/navigation');
