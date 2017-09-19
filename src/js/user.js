368527d7124f34026252594c634a840bea62e73d
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-09-19 18:11:51 +0300

    fix auth

--- src/js/user.js
+++ src/js/user.js
@@ -5,18 +5,17 @@ class User {
         this.authenticated = false;
     }
 
-    is_authenticated(callback) {
+    is_authenticated(callback = () =>{}) {
         Http.Get('/current', function (error, response) {
             window.User.authenticated = error === null;
             callback();
-
         });
     }
 
     sign_up(login, email, password, callback) {
         Http.Post('/signup', {login, email, password},
             function (xhr) {
-                window.User.authenticated = true;
+                window.User.is_authenticated(callback);
                 callback(xhr);
             }
         );
@@ -25,7 +24,7 @@ class User {
     login(login, password, callback) {
         Http.Post('/signin', {login, password},
             function (xhr) {
-                window.User.authenticated = true;
+                window.User.is_authenticated(callback);
                 callback(xhr);
             });
     }
