1278c81574926a60a93593e77ef958d748a1340b
Nikolay <yaminsky96@gmail.com>
2017-10-21 11:21:34 +0300

    base views

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import Router from './modules/router';
 import SigninView from './views/signin-view/signin';
 import AboutView from './views/about-view/about';
 import TopView from './views/top-view/top';
+import SignupView from './views/signup-view/signup';
 
 (() => {
 
@@ -16,7 +17,8 @@ import TopView from './views/top-view/top';
     router
         .addRoute('/', SigninView)
         .addRoute('/about', AboutView)
-        .addRoute('/top', TopView);
+        .addRoute('/top', TopView)
+        .addRoute('/signup', SignupView);
     router.start();
 
 })();
--- src/views/top-view/top.js
+++ src/views/top-view/top.js
@@ -4,7 +4,14 @@ class TopView extends BaseView {
 
     getTemplate() {
         const template =require('./top.pug');
-        return template();
+        let data = {users:[
+            {name: 'John', score: '82'},
+            {name: 'Billy', score: '33'},
+            {name: 'Klark', score: '28'},
+            {name: 'Bob', score: '16'},
+            {name: 'Kerson', score: '10'}
+        ]};
+        return template(data);
     }
 
 }
